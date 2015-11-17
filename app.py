d3926c28d06911e91bac246cd72c0e8c1d0c8225
thoppe <travis.hoppe@gmail.com>
2015-11-17 14:25:41 -0500

    Refactor app, remove cruft.

--- app.py
+++ app.py
@@ -7,25 +7,24 @@ app = flask.Flask(__name__)
 
 import absurd_noun_pairs as ANP
 
+_default_return_results = 15
+
+def get_results():
+    result_list = []
+    for k in xrange(_default_return_results):
+        phrase, scores = ANP.quality_filter()
+        result_list.append(' '.join(phrase))
+    return result_list
 
 @app.route('/', methods=['GET'])
 def front_page():
-    global button_presses
     
     args = {}
-    args["title"]      = "Colorless green ideas"
-    args["author"]     = "travis hoppe"
-    args["author_url"] = "http://thoppe.github.io"
+    args["title"]       = "Colorless green ideas"
+    args["author"]      = "travis hoppe"
     args["project_url"] = "https://github.com/thoppe/Colorless-Green-Ideas"
-    args["button_status"] = "You have not pressed the button."
-
-    result_list = []
-
-    for k in xrange(15):
-        phrase, scores = ANP.quality_filter()
-        result_list.append(' '.join(phrase))
-
-    args["result_list"] = result_list
+    args["result_list"] = get_results()
+    
     return flask.render_template('index.html', **args)
 
 if __name__ == '__main__':
