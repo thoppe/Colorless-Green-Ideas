a4c2d34189a831ab853c2a9c4c360fc5dfcec197
thoppe <travis.hoppe@gmail.com>
2015-11-17 14:38:56 -0500

    Added API access

--- app.py
+++ app.py
@@ -9,12 +9,19 @@ import absurd_noun_pairs as ANP
 
 _default_return_results = 15
 
-def get_results():
+def get_results(n):
     result_list = []
-    for k in xrange(_default_return_results):
+    for k in xrange(n):
         phrase, scores = ANP.quality_filter()
         result_list.append(' '.join(phrase))
     return result_list
+
+@app.route('/API', defaults={"n" : _default_return_results})
+@app.route('/API/<int:n>', methods=['GET'])
+def text_results(n = _default_return_results):
+    n = max(1,min(n,50))
+    results = {"results":get_results(n)}
+    return flask.jsonify(results)
 
 @app.route('/', methods=['GET'])
 def front_page():
@@ -23,7 +30,7 @@ def front_page():
     args["title"]       = "Colorless green ideas"
     args["author"]      = "travis hoppe"
     args["project_url"] = "https://github.com/thoppe/Colorless-Green-Ideas"
-    args["result_list"] = get_results()
+    args["result_list"] = get_results(_default_return_results)
     
     return flask.render_template('index.html', **args)
 
